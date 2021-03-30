27f4b20e239199d184b2c16e8fcaddfcb7e306d4
pinakiganguly <59434679+pinakiganguly@users.noreply.github.com>
2021-03-30 17:40:07 +0530

    Add files via upload

---
+++ Thread/deadlock.java
@@ -0,0 +1,56 @@
+package Pinaki1;
+  
+class A{
+	synchronized void foo(B b) {
+		String name=Thread.currentThread().getName();
+		System.out.println(name+" entered A.foo");
+		try {
+			Thread.sleep(1000);
+		}
+		catch(Exception e){
+			System.out.println("A Interrupted");
+		}
+		System.out.println(name+"trying to call B.last()");
+		b.last();
+	}
+	synchronized void last() {
+		System.out.println("Inside A.last");
+	}
+}
+class B{
+	synchronized void bar(A a) {
+		String name=Thread.currentThread().getName();
+		System.out.println(name+" entered B.bar");
+	try {
+		Thread.sleep(1000);
+	}catch(Exception e) {
+		System.out.println("B interrupted");
+	}
+	System.out.println(name+" trying to call A.last()");
+	a.last();
+	}
+	synchronized void last() {
+		System.out.println("Inside A.last()");
+	}
+}
+public class deadlock implements Runnable{
+	A a=new A();
+	B b=new B();
+	deadlock(){
+		Thread.currentThread().setName("Main Thread");
+		Thread t=new Thread(this,"Racing Thread");
+		t.start();
+		
+		a.foo(b);
+		System.out.println("Back in main Thread");
+	}
+	public void run() {
+		b.bar(a);
+		System.out.println("Back in other thread");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new deadlock();
+	}
+
+}
